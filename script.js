20494ef4ae1de61744fcb3bbd5fc26799e5e63a6
Popova Olga <686371olia@mail.ru>
2022-03-08 01:03:12 +0300

    Добавление кнопке e_btn свойства display: none

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const btn = document.querySelector('#btn');
 const inputText = document.querySelector('input[type=text]');
 const square = document.querySelector('#square');
 
+const eBtn = document.querySelector('#e_btn');
+
 const changeColor = function(event) {
   square.style.backgroundColor = inputText.value;
 };
 
 btn.addEventListener('click', changeColor);
+
+eBtn.style.display = 'none';
