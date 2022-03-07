59733fe50f438dd7b917425fb5baa68f105c8f82
Popova Olga <686371olia@mail.ru>
2022-03-08 02:29:52 +0300

    Изменение ширины и высоты круга в зависимости от значения ползунка

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 const btn = document.querySelector('#btn');
 const inputText = document.querySelector('input[type=text]');
 const square = document.querySelector('#square');
-
+const range = document.querySelector('input[type=range]');
 const eBtn = document.querySelector('#e_btn');
+const circle = document.querySelector('#circle');
 
 const changeColor = function(event) {
   square.style.backgroundColor = inputText.value;
 };
 
-btn.addEventListener('click', changeColor);
+const changeCircle = function(event) {
+  circle.style.width = event.target.value + '%';
+  circle.style.height = event.target.value + '%';
+};
 
 eBtn.style.display = 'none';
+btn.addEventListener('click', changeColor);   
+range.addEventListener('input', changeCircle);   
+
